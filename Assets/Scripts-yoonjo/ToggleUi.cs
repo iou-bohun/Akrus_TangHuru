b4534d5d5bbaafe8a706f1eb5c78f9c62f6a5cd9
baptong <130282352+baptong@users.noreply.github.com>
2023-10-27 16:27:02 +0900

    ToggleUi 스크립트생성

--- Assets/Scripts-yoonjo/ToggleUi.cs
+++ Assets/Scripts-yoonjo/ToggleUi.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SwitchToggle : MonoBehaviour
+public class ToggleUi : MonoBehaviour
 {
     [SerializeField] RectTransform uiHandleRectTransform;
     [SerializeField] Color backgroundActiveColor;
