3fbe8b70bdfc3d49fb3a61782312bcbf1dc2de95
Gyungmi <cho_c0@naver.com>
2023-10-31 17:17:51 +0900

    생산소-열매생성

--- Assets/Gyungmi/FruitSpawner.cs
+++ Assets/Gyungmi/FruitSpawner.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class FruitSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 과일 스폰포인트를 담을 배열
+    public Transform[] points;
+
+    // 딸기 프리팹
+    public GameObject strawberryPrefab;
+
+    // 딸기 생성 주기
+    float createTime = 5f;
+    // 최대 갯수
+    int maxNum = 8;
+    
     void Start()
     {
-        
+        points = GameObject.Find("FruitSpawnPoint").GetComponentsInChildren<Transform>();
+
+        if (points.Length > 0)
+        {
+            StartCoroutine(this.SpawnFruit());
+        }
     }
 
-    // Update is called once per frame
+    IEnumerator SpawnFruit()
+    {
+        while(true)
+        {
+            int fruitCount = (int)GameObject.FindGameObjectsWithTag("Fruit").Length;
+
+            if (fruitCount < maxNum)
+            {
+                yield return new WaitForSeconds(createTime);
+
+                int idx = Random.Range(1, points.Length);
+
+                Instantiate(strawberryPrefab, points[idx].position, points[idx].rotation);
+
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+
+    }
+
     void Update()
     {
         
