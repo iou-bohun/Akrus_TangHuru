10d916616bdf9927bd4a8e251ef45f7781fb9585
Gyungmi <cho_c0@naver.com>
2023-11-03 03:23:20 +0900

    과일 랜덤 종류 생성

--- Assets/Gyungmi/Container.cs
+++ Assets/Gyungmi/Container.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class Container : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static Container instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static Container Instance 
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
