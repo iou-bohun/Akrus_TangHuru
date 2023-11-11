242eb4d3fb64dbfcf1ff4d9c0e396181adaafd35
bohun <slawkd2002@gmail.com>
2023-11-11 23:57:08 +0900

    commit

--- Assets/Bohuh/B_RestingContainer.cs
+++ Assets/Bohuh/B_RestingContainer.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class B_RestingContainer : MonoBehaviour
 {
-   
+    [SerializeField] GameObject strawberryTangHuru;
+    private void Update()
+    {
+        if(DataManager.Instance.strawberryTangHuru >= 1)
+        {
+            strawberryTangHuru.SetActive(true);
+        }
+        else
+        {
+            strawberryTangHuru.SetActive(false);
+        }
+    }
 }
