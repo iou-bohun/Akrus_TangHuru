1f6b873f0ed0b00961dd09f40b18cc37bb2a637a
bohun <slawkd2002@gmail.com>
2023-11-16 03:22:22 +0900

    과일별 진열대 생성

--- Assets/Bohuh/Scripts/B_RestingContainer.cs
+++ Assets/Bohuh/Scripts/B_RestingContainer.cs
@@ -6,15 +6,73 @@ using UnityEngine;
 public class B_RestingContainer : MonoBehaviour
 {
     [SerializeField] GameObject strawberryTangHuru;
+    [SerializeField] GameObject grapeTang;
+    [SerializeField] GameObject orangeTang;
+    [SerializeField] GameObject pineappaleTang;
+    [SerializeField] GameObject blueberryTang;
+
     private void Update()
     {
-        if(DataManager.Instance.strawberryTangHuru >= 1)
+        sUpdate();
+        gUpdate();
+        oUpdate();
+        pUpdate();
+        bUpdate ();
+    }
+
+    private void sUpdate()
+    {
+        if (DataManager.Instance.strawberryTangHuru >= 1)
         {
             strawberryTangHuru.SetActive(true);
         }
         else
         {
             strawberryTangHuru.SetActive(false);
+        }
+      }
+       private void gUpdate()
+        {
+            if (DataManager.Instance.grapeTangHuru >= 1)
+            {
+            grapeTang.SetActive(true);
+            }
+            else
+            {
+            grapeTang.SetActive(false);
+            }
+        }
+    private void oUpdate()
+    {
+        if (DataManager.Instance.orangeTangHuru >= 1)
+        {
+            orangeTang.SetActive(true);
+        }
+        else
+        {
+            orangeTang.SetActive(false);
+        }
+    }
+    private void pUpdate()
+    {
+        if (DataManager.Instance.pineappleTangHuru >= 1)
+        {
+            pineappaleTang.SetActive(true);
+        }
+        else
+        {
+            pineappaleTang.SetActive(false);
+        }
+    }
+    private void bUpdate()
+    {
+        if (DataManager.Instance.blueberryTangHuru >= 1)
+        {
+            blueberryTang.SetActive(true);
+        }
+        else
+        {
+            blueberryTang.SetActive(false);
         }
     }
 }
--- Assets/Bohuh/Scripts/TangHuruDisplay.cs
+++ Assets/Bohuh/Scripts/TangHuruDisplay.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class TangHuruDisplay : MonoBehaviour
 {
     [SerializeField] Transform[] spawnPoints;
-    public GameObject prefab;
+    public GameObject[] prefab;
     public GameObject alert;
     int spawnPoint = 0;
 
     public  void DisplayStrawberryTangHuru()
     {
+        SoundManager.Instance.PlayTapSound();
         if (DataManager.Instance.sellingTangHuru >= 6)
         {
             Debug.Log("판매대가 모자랍니다");
@@ -25,31 +26,89 @@ public class TangHuruDisplay : MonoBehaviour
             DataManager.Instance.strawberryTangHuru--;
             DataManager.Instance.sellingStarwberryTangHuru++;
             Debug.Log("소환");
-            Instantiate(prefab, spawnPoints[DataManager.Instance.sellingTangHuru].position, spawnPoints[DataManager.Instance.sellingTangHuru].rotation);
+            Instantiate(prefab[0], spawnPoints[DataManager.Instance.sellingTangHuru].position, spawnPoints[DataManager.Instance.sellingTangHuru].rotation);
             DataManager.Instance.sellingTangHuru++;
         }
     }
     public void DisplayGrapeTangHuru()
     {
         SoundManager.Instance.PlayTapSound();
-        DataManager.Instance.grapeTangHuru++;
+        if (DataManager.Instance.sellingTangHuru >= 6)
+        {
+            Debug.Log("판매대가 모자랍니다");
+            alert.SetActive(true);
+            StartCoroutine("AlertDelay");
+            return;
+        }
+        if (DataManager.Instance.grapeTangHuru >= 1)
+        {
+            SoundManager.Instance.PlayTapSound();
+            DataManager.Instance.grapeTangHuru--;
+            DataManager.Instance.sellingGrapeTangHuru++;
+            Debug.Log("소환");
+            Instantiate(prefab[1], spawnPoints[DataManager.Instance.sellingTangHuru].position, spawnPoints[DataManager.Instance.sellingTangHuru].rotation);
+            DataManager.Instance.sellingTangHuru++;
+        }
     }
     public void DisplayOrangeTangHuru()
     {
         SoundManager.Instance.PlayTapSound();
+        if (DataManager.Instance.sellingTangHuru >= 6)
+        {
+            Debug.Log("판매대가 모자랍니다");
+            alert.SetActive(true);
+            StartCoroutine("AlertDelay");
+            return;
+        }
+        if (DataManager.Instance.orangeTangHuru >= 1)
+        {
+            SoundManager.Instance.PlayTapSound();
+            DataManager.Instance.orangeTangHuru--;
+            DataManager.Instance.sellingOrangeTangHuru++;
+            Debug.Log("소환");
+            Instantiate(prefab[2], spawnPoints[DataManager.Instance.sellingTangHuru].position, spawnPoints[DataManager.Instance.sellingTangHuru].rotation);
+            DataManager.Instance.sellingTangHuru++;
+        }
     }
     public void DispalyPineappleTangHuru()
     {
         SoundManager.Instance.PlayTapSound();
+        if (DataManager.Instance.sellingTangHuru >= 6)
+        {
+            Debug.Log("판매대가 모자랍니다");
+            alert.SetActive(true);
+            StartCoroutine("AlertDelay");
+            return;
+        }
+        if (DataManager.Instance.pineappleTangHuru >= 1)
+        {
+            SoundManager.Instance.PlayTapSound();
+            DataManager.Instance.pineappleTangHuru--;
+            DataManager.Instance.sellingPineappleTangHuru++;
+            Debug.Log("소환");
+            Instantiate(prefab[3], spawnPoints[DataManager.Instance.sellingTangHuru].position, spawnPoints[DataManager.Instance.sellingTangHuru].rotation);
+            DataManager.Instance.sellingTangHuru++;
+        }
     }
     public void DisaplyBlueberryTangHuru()
     {
         SoundManager.Instance.PlayTapSound();
-    }
-
-    public void DisplayTang(int value)
-    {
-
+        if (DataManager.Instance.sellingTangHuru >= 6)
+        {
+            Debug.Log("판매대가 모자랍니다");
+            alert.SetActive(true);
+            StartCoroutine("AlertDelay");
+            return;
+        }
+        if (DataManager.Instance.blueberryTangHuru >= 1)
+        {
+            SoundManager.Instance.PlayTapSound();
+            DataManager.Instance.blueberryTangHuru--;
+            DataManager.Instance.SellingBlueberryTangHuru++;
+            Debug.Log("소환");
+            Instantiate(prefab[4], spawnPoints[DataManager.Instance.sellingTangHuru].position, spawnPoints[DataManager.Instance.sellingTangHuru].rotation);
+            DataManager.Instance.sellingTangHuru++;
+        }
     }
 
     IEnumerator AlertDelay()
