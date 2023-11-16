26316b77eb1144daa457974c25460c4b0d4f3bfd
baptong <130282352+baptong@users.noreply.github.com>
2023-11-16 20:57:11 +0900

    레벨 고치는중

--- Assets/Bohuh/Scripts/SellingProgressBar.cs
+++ Assets/Bohuh/Scripts/SellingProgressBar.cs
@@ -4,17 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using TMPro;
+using System;
 
 public class SellingProgressBar : MonoBehaviour
 {
+    public TMP_Text levelUIText;
+    public TMP_Text levelText;
+    public TMP_Text expText;
+
     [SerializeField] UnityEngine.UI.Slider slider;
+    [SerializeField] UnityEngine.UI.Slider expbar;
+
     float curTime = 30;
     float maxTime = 30;
 
+    float maxExp = 3;
+    float curExp = 0;
+
+    public int level = 1;
 
     private void OnEnable()
     {
         curTime = (float)maxTime;
+        expbar.value = (float)curExp / (float)maxExp; // Exp의 값을 0/100으로 시작
     }
 
     private void Update()
@@ -54,7 +67,32 @@ public class SellingProgressBar : MonoBehaviour
                 DataManager.Instance.sellingTanghurus.Remove(tangHuru);
                 Destroy(tangHuru);
             }
-            
+            HandleExp(); // 경험치 및 레벨 관련 기능 호출
         }
+    }
+
+    private void HandleExp()
+    {
+        Debug.Log("경험치 증가");
+        curExp += 1;
+
+        expbar.value = (float)curExp / (float)maxExp; // Handle의 값 0/100
+
+        if (expbar.value >= 1)
+        {
+            expbar.value = expbar.value - 1;
+            level++;
+            levelUIText.text = level.ToString();
+            levelText.text = level.ToString();
+
+            if (level <= 6)
+            {
+                maxExp += 5;
+            }
+            else
+                maxExp += 6;
+        }
+
+        expText.text = curExp.ToString() + "/" + maxExp.ToString();
     }
 }
